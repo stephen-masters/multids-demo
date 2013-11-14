944e101a39a5e3dfa166fbdb651919111d956a62
Stephen Masters <steve@scattercode.co.uk>
2013-11-14 10:33:02 +0000

    Removing unused imports

--- src/main/java/com/sctrcd/multidsdemo/integration/repositories/bar/BarRepository.java
+++ src/main/java/com/sctrcd/multidsdemo/integration/repositories/bar/BarRepository.java
@@ -1,7 +1,5 @@
 package com.sctrcd.multidsdemo.integration.repositories.bar;
 
-import javax.persistence.PersistenceContext;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sctrcd.multidsdemo.domain.bar.Bar;
--- src/main/java/com/sctrcd/multidsdemo/integration/repositories/foo/FooRepository.java
+++ src/main/java/com/sctrcd/multidsdemo/integration/repositories/foo/FooRepository.java
@@ -1,7 +1,5 @@
 package com.sctrcd.multidsdemo.integration.repositories.foo;
 
-import javax.persistence.PersistenceContext;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sctrcd.multidsdemo.domain.foo.Foo;
